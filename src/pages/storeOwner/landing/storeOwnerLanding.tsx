d8cd6543e98f743eafd95276a6b7781c05c27049
Anurag M K <109131413+Anurag-M-K@users.noreply.github.com>
2024-07-15 07:04:34 +0530

    storeowner landing

--- src/pages/storeOwner/landing/storeOwnerLanding.tsx
+++ src/pages/storeOwner/landing/storeOwnerLanding.tsx
@@ -17,11 +17,10 @@ import IconCashBanknotes from '../../../components/Icon/IconCashBanknotes';
 import { HiOutlineStatusOnline } from 'react-icons/hi';
 
 function StoreOwnerLanding() {
-    const { storeOwnerToken } = useSelector((state:any) => state.storeOwner);
-    const { storeOwnerData } = useSelector((state:any) => state.storeOwner);
+    const { storeOwnerToken } = useSelector((state: any) => state.storeOwner);
+    const { storeOwnerData } = useSelector((state: any) => state.storeOwner);
     const dispatch = useDispatch();
-    const navigate = useNavigate()
-    console.log('storeowner data ', storeOwnerData);
+    const navigate = useNavigate();
     const fetchStoreData = async () => {
         const res = await fetchStore(storeOwnerToken);
         dispatch(setstoreOwnerData(res?.data));
@@ -30,10 +29,7 @@ function StoreOwnerLanding() {
         fetchStoreData();
     }, []);
 
- 
-
     const calculateDaysLeft = (expirationDate: any) => {
-        console.log("Exiprationdat ",expirationDate)
         const currentDate: any = new Date();
         const expiryDate: any = new Date(expirationDate);
         const timeDifference = expiryDate - currentDate;
@@ -41,10 +37,9 @@ function StoreOwnerLanding() {
         return daysLeft;
     };
 
-    const daysleft = calculateDaysLeft(storeOwnerData?.subscriptionExpiresAt);
-
-        const whatsappNumber = import.meta.env.VITE_APP_ADMIN_PHONE_NUMBER
-        const message = "Hi, I want to upgrade my store plan.";
+    const daysleft = calculateDaysLeft(storeOwnerData?.subscription?.expiresAt);
+    const whatsappNumber = import.meta.env.VITE_APP_ADMIN_PHONE_NUMBER;
+    const message = 'Hi, I want to upgrade my store plan.';
     return (
         <div className="grid grid-cols-1  md:grid-cols-3 gap-x-4">
             <div className="panel shadow-md">
@@ -109,62 +104,66 @@ function StoreOwnerLanding() {
             <div>
                 <div className="shadow-md p-5 panel">
                     <div className="flex items-center justify-between mb-10">
-                        <h5 className="font-semibold text-lg dark:text-white-light">
-                            {' '}
-                            {storeOwnerData?.subscriptionPlan === 'basic' ? 'Basic plan' : storeOwnerData?.subscriptionPlan === 'premium' ? 'Premium Plan' : 'Free Plan'}
-                        </h5>
-                        {
-                            daysleft < 420 && (
-                                <a href={`https://wa.me/${whatsappNumber}?text=${encodeURIComponent(message)}`} className="btn btn-primary">Renew Now</a>
-                            )
-                        }
+                        <h5 className="font-semibold uppercase text-lg dark:text-white-light"> {storeOwnerData?.subscription?.plan?.name} plan</h5>
+                        {daysleft < 20 && (
+                            <a href={`https://wa.me/${whatsappNumber}?text=${encodeURIComponent(message)}`} className="btn btn-primary">
+                                Renew Now
+                            </a>
+                        )}
                     </div>
                     <div className="group">
+                        <h1 className="list-inside list-disc text-white-dark font-semibold mb-7 ">Premium plan :</h1>
                         <ul className="list-inside list-disc text-white-dark font-semibold mb-7 space-y-2">
-                            <li>Add upto 5 images for each product</li>
+                            <li> Add upto 4 images for each product</li>
                             <li>Unlimited Reports</li>
                             <li>One year time period</li>
                         </ul>
-                        {daysleft > 0 && (
-
-                            <div className="flex items-center justify-between mb-4 font-semibold">
-                            <p className="flex items-center rounded-full bg-dark px-2 py-1 text-xs text-white-light font-semibold">
-                                <IconClock className="w-3 h-3 ltr:mr-1 rtl:ml-1" />{daysleft} Days Left
-                            </p>
-                        </div>
-                        ) }
-                        <div className="rounded-full h-2.5 p-0.5 bg-dark-light overflow-hidden mb-5 dark:bg-dark-light/10">
-                            <div
-                                className="bg-gradient-to-r from-[#f67062] to-[#fc5296] w-full h-full rounded-full relative"
-                                style={{
-                                    width: `${
-                                        daysleft === 0
-                                            ? '0%'
-                                            : daysleft < 29
-                                            ? '8%'
-                                            : daysleft < 54
-                                            ? '15%'
-                                            : daysleft < 91
-                                            ? '25%'
-                                            : daysleft < 127
-                                            ? '35%'
-                                            : daysleft < 164
-                                            ? '45%'
-                                            : daysleft < 200
-                                            ? '55%'
-                                            : daysleft < 239
-                                            ? '65%'
-                                            : daysleft < 273
-                                            ? ' 75%'
-                                            : daysleft < 310
-                                            ? '85%'
-                                            : daysleft < 365
-                                            ? '100%'
-                                            : ''
-                                    }`,
-                                }}
-                            ></div>
-                        </div>
+                        {storeOwnerData?.subscription?.plan?.name === 'premium' && (
+                            <>
+                                <>
+                                    {daysleft > 0 && (
+                                        <div className="flex items-center justify-between mb-4 font-semibold">
+                                            <p className="flex items-center rounded-full bg-dark px-2 py-1 text-xs text-white-light font-semibold">
+                                                <IconClock className="w-3 h-3 ltr:mr-1 rtl:ml-1" />
+                                                {daysleft} Days Left
+                                            </p>
+                                        </div>
+                                    )}
+                                </>
+                                <div className="rounded-full h-2.5 p-0.5 bg-dark-light overflow-hidden mb-5 dark:bg-dark-light/10">
+                                    <div
+                                        className="bg-gradient-to-r from-[#f67062] to-[#fc5296] w-full h-full rounded-full relative"
+                                        style={{
+                                            width: `${
+                                                daysleft === 0
+                                                    ? '0%'
+                                                    : daysleft < 29
+                                                    ? '8%'
+                                                    : daysleft < 54
+                                                    ? '15%'
+                                                    : daysleft < 91
+                                                    ? '25%'
+                                                    : daysleft < 127
+                                                    ? '35%'
+                                                    : daysleft < 164
+                                                    ? '45%'
+                                                    : daysleft < 200
+                                                    ? '55%'
+                                                    : daysleft < 239
+                                                    ? '65%'
+                                                    : daysleft < 273
+                                                    ? ' 75%'
+                                                    : daysleft < 310
+                                                    ? '85%'
+                                                    : daysleft < 365
+                                                    ? '100%'
+                                                    : ''
+                                            }`,
+                                        }}
+                                    ></div>
+                                </div>
+                            </>
+                        )}
                     </div>
                 </div>
             </div>
